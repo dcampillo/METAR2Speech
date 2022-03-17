109b1d670df49445e802e5a306f16ed3771f7145
David Campillo <david@zorglub.info>
2022-03-17 13:33:01 +0100

    Added comment to program.py

--- program.py
+++ program.py
@@ -1,13 +1,16 @@
 import metar2speech
 
+# Create an instace of the Converter
 m2s = metar2speech.Converter()
 
+# Retrieve METAR
 m2s.GetMetar("lsmp")
 
+#Print the METAR as downloaded
 print(m2s.rawMetar)
-print(m2s.lastICAO)
+
+# Print the METAR converted to speakable version by gTTs
 print(m2s.voiceTextMetar)
-print(m2s.decodedMetar.wind)
 
-
+# Generate and save the METAR in a mp3 file
 m2s.SaveMetarToAudio("c:\\temp\\")
